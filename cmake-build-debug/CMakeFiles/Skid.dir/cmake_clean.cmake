1d96887d76c5dc01bf1c77654778331a6afce781
CXCubeHD <cxcubehd@gmail.com>
2021-09-07 17:28:58 +0200

    .

---
+++ cmake-build-debug/CMakeFiles/Skid.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Skid.dir/library.cpp.obj"
+  "Skid.dll"
+  "Skid.dll.manifest"
+  "Skid.lib"
+  "Skid.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Skid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Skid.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Skid.
+# This may be replaced when dependencies are built.
